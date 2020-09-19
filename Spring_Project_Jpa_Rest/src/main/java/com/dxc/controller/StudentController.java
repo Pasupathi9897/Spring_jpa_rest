d1587505b517a6bfc1e06d7f18257c1fa3b44b59
paduri2 <paduri2@IN-5CG0251N43>
2020-09-19 12:42:25 +0530

    spring jpa rest

---
+++ Spring_Project_Jpa_Rest/src/main/java/com/dxc/controller/StudentController.java
@@ -0,0 +1,24 @@
+package com.dxc.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dxc.dao.StudentRepository;
+import com.dxc.model.Student;
+
+@RestController
+public class StudentController {
+
+	@Autowired
+	StudentRepository studentRepository;
+	
+	@GetMapping("students")
+	public List<Student> getStudents()
+	{
+		List<Student> students = (List<Student>) studentRepository.findAll();
+		return students;
+	}
+}
